498a6dd8a721c0331b1f272a2ebbaefcc64fb0b8
MrTriggerMcFragger <78004809+MrTriggerMcFragger@users.noreply.github.com>
2023-11-03 14:35:38 -0400

    Initial commit

---
+++ BlueteethInternalNetworkStack.h
@@ -0,0 +1,29 @@
+
+#ifndef BlueteethInternalNetworkStack_h
+#define BlueteethInternalNetworkStack_h
+
+class BlueteethBaseStack
+{
+
+public:
+    BlueteethBaseStack(){
+    }
+
+protected:
+
+
+};
+
+class BlueteethMasterStack: public BlueteethBaseStack {
+
+public:
+
+    BlueteethMasterStack():BlueteethBaseStack(){      
+    }
+
+
+private:
+
+};
+
+#endif
