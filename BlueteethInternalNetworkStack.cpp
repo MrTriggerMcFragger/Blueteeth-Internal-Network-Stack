c1f7a699ab444152f8c421cacf8c0b48f72113e6
MrTriggerMcFragger <78004809+MrTriggerMcFragger@users.noreply.github.com>
2023-11-18 20:14:30 -0500

    Fixed issues with the control plane and added initial code for the dataplane (currently just for testing)

--- BlueteethInternalNetworkStack.cpp
+++ BlueteethInternalNetworkStack.cpp
@@ -1,78 +1,123 @@
 #include <BlueteethInternalNetworkStack.h>
 
-extern BlueteethBaseStack * internalNetworkStack;
+extern BlueteethBaseStack * internalNetworkStackPtr;
+
+
+void inline flushSerialBuffer(HardwareSerial * serial){
+    while (serial -> available() > 0){
+        serial -> read();
+    }
+}
+
 
 /* Grab contents from serial buffer and package them into a blueteeth packet
 *
 *  @serial - The serial port being read from
 *  @return - The blueteeth packet received from the buffer
 */
-blueTeethPacket retrievePacketFromBuffer(HardwareSerial * serial){
+BlueteethPacket retrievePacketFromBuffer(HardwareSerial * serial){
 
     //Have to temporarily grab the data form the UART buffer into a byte buffer (so it can be packaged in the packet struct)
-    uint8_t buffer [sizeof(blueTeethPacket)];
-    serial -> readBytes(buffer, sizeof(buffer));
+    // uint8_t buffer [sizeof(blueTeethPacket)];
+    // serial -> readBytes(buffer, sizeof(buffer));
     
     //Package the packet struct with data from buffer
-    blueTeethPacket retrievedPacket;
-    retrievedPacket.tokenFlag = buffer[0];
-    retrievedPacket.srcAddr = buffer[1];
-    retrievedPacket.dstAddr = buffer[2];
-    retrievedPacket.type = buffer[3];
-    for (int i = 4; i < sizeof(blueTeethPacket); i++){
-        retrievedPacket.payload[i-4] = buffer[i];
+    BlueteethPacket retrievedPacket;
+    retrievedPacket.tokenFlag = serial -> read();
+    retrievedPacket.srcAddr = serial -> read();
+    retrievedPacket.dstAddr = serial -> read();
+    retrievedPacket.type = serial -> read();
+    for (int i = 0; i < MAX_PAYLOAD_SIZE; i++){
+        retrievedPacket.payload[i] = serial -> read();
     }
     return retrievedPacket;
+
 }
 
-void packetReceived(){
+void uartFrameReceived(){
     
+    int bytesInBuffer = internalNetworkStackPtr -> controlPlane -> available();
+
     //Don't try to package packet unless there's enough data on the buffer to do so
-    if(internalNetworkStack -> controlPlane -> available() > sizeof(blueTeethPacket)){  
-        
-        blueTeethPacket receivedPacket = retrievePacketFromBuffer(internalNetworkStack -> controlPlane);
+    if(bytesInBuffer < sizeof(BlueteethPacket)){
+        Serial.print("Not enough bits for a packet\n\r"); //DEBUG STATEMENT 
+        return;
+    }  
+    //If there are extra bytes in the buffer, something is wrong, and the buffer has been corrupted.
+    /*  
+        This seems to prevent corruption as the serial callback doesn't get called EVERY time 
+        a new frame is received, but after a burst of frames ends. Maybe move into the prior conditional. 
+    */
+    else if ((bytesInBuffer % sizeof(BlueteethPacket)) != 0 ) 
+    {
+        Serial.print("Buffer corrupted\n\r");
+        flushSerialBuffer(internalNetworkStackPtr -> controlPlane);
+        return;
+    }      
+    
+    BlueteethPacket receivedPacket = retrievePacketFromBuffer(internalNetworkStackPtr -> controlPlane);
 
-        //Assess the packet
-        //If the token was received, transmit items in queue.
-        if (receivedPacket.tokenFlag == 1){
-            while(uxQueueMessagesWaiting(internalNetworkStack -> transmitPacketBuffer) > 0){
-                blueTeethPacket transmitPacket;
-                xQueueReceive(internalNetworkStack -> transmitPacketBuffer, &transmitPacket, 0);
-                internalNetworkStack -> transmitPacket(transmitPacket);
-            }
+    // Serial.printf("[%lu - Rx Packet] t = %u, src = %u, dst = %u, type = %u, payload = %s\n\r", millis(), receivedPacket.tokenFlag, receivedPacket.srcAddr, receivedPacket.dstAddr, receivedPacket.type, (char*) receivedPacket.payload); //DEBUG STATEMENT
 
-            blueTeethPacket tokenPacket;
-            tokenPacket.tokenFlag = 1;
-            internalNetworkStack -> transmitPacket(tokenPacket);
-        }
-        //If instead the packet was meant for this device, add it to the buffer
-        else if (receivedPacket.dstAddr == internalNetworkStack -> address){
-            xQueueSend(internalNetworkStack -> receivedPacketBuffer, &receivedPacket, 0);
-        }
-        else if (receivedPacket.dstAddr == 255){ //Packets with destination address 255 are intended to intialize node addresses
-            switch(internalNetworkStack -> address){ 
-                case 0: //0 is the master (who sends the intialization packet). Once the packet makes it all the way back to the sender, the initilaization phase is done (so add the packet to the number of slaves can be inferred)
-                    xQueueSend(internalNetworkStack -> receivedPacketBuffer, &receivedPacket, 0);
-                    break; 
-                case 255: 
-                    internalNetworkStack -> address = receivedPacket.payload[0];
-                    receivedPacket.payload[0]++; //increment the address value and pass it on
-                default: //Default response if a node has been assigned an address is to pass on the intializaton packet
-                    internalNetworkStack -> transmitPacket(receivedPacket);
-                    break;
+    //Assess the packet
+    //If the token was received, transmit items in queue.
+    if (receivedPacket.tokenFlag == 1){
+        // Serial.print("Token packet received\n\r"); //DEBUG STATEMENT 
+        internalNetworkStackPtr -> tokenReceived();
+    }
+    else if (receivedPacket.dstAddr == 255){ //Packets with destination address 255 are intended to intialize node addresses
+        Serial.print("Received initializaiton packet\n\r"); //DEBUG STATEMENT
+        internalNetworkStackPtr -> initializationReceived(receivedPacket);
+    }
+    //If the packet was sent from this device, throw it away.
+    else if (receivedPacket.srcAddr == internalNetworkStackPtr -> address){
+        // Serial.print("Received my own packet\n\r"); //DEBUG STATEMENT 
+        //Do nothing
+    }
+    else if (receivedPacket.dstAddr == 254){ //Packets with destination address 254 are intended for broadcasts
+        // Serial.print("Received broadcast packet\n\r"); //DEBUG STATEMENT 
+        internalNetworkStackPtr -> queuePacket(false, receivedPacket);
+        xQueueSend(internalNetworkStackPtr -> receivedPacketBuffer, &receivedPacket, 0);
+        vTaskResume(*(internalNetworkStackPtr -> receiveTaskCallback));
+    }
+    //If instead the packet was meant for this device, add it to the buffer
+    else if (receivedPacket.dstAddr == internalNetworkStackPtr -> address){
+        // Serial.print("Received packet for myself\n\r"); //DEBUG STATEMENT 
+        xQueueSend(internalNetworkStackPtr -> receivedPacketBuffer, &receivedPacket, 0);
+        vTaskResume(*(internalNetworkStackPtr -> receiveTaskCallback));
+    }
+    //If the packet was not meant for this device at all, send it on to the next device in the ring
+    else {
+        Serial.print("Received packet for someone else\n\r"); //DEBUG STATEMENT
+        internalNetworkStackPtr -> transmitPacket(receivedPacket);
+    }
+}
 
-            }
-        }
-        //If the packet was not meant for this device at all, send it on to the next device in the ring
-        else if (receivedPacket.dstAddr != internalNetworkStack -> address){
-            internalNetworkStack -> transmitPacket(receivedPacket);
-        }
+void dataStreamReceived(){
+
+    static int totalBytes = 0;
+    static uint32_t time = 0;
+    int newBytes = internalNetworkStackPtr -> dataPlane -> available();
+    
+    if (totalBytes == 0) {
+        time = millis();
     }
 
-}
+    if (totalBytes + newBytes > MAX_DATA_BUFFER_SIZE){
+        newBytes = MAX_DATA_BUFFER_SIZE - totalBytes;
+    }
 
-void blueTeethNetworkSetup(){
-    
-    internalNetworkStack -> controlPlane -> onReceive(packetReceived);
+    //TEMPORARY!!!
+    //Eventually need to make sure buffer doesn't overflow.
+    internalNetworkStackPtr -> dataPlane -> readBytes(internalNetworkStackPtr -> dataBuffer + totalBytes, newBytes);
 
+    totalBytes += newBytes;
+
+    if (totalBytes == 40000){ //DEBUG STATEMENT
+        time = millis() - time;
+        Serial.printf("Stream successful! Time elapsed (ms) = %lu\n\r", time);
+        totalBytes = 0;
+    } 
+
+    // Serial.printf("Data received: %d\n\r", totalBytes); //DEBUG STATEMENT
 }
--- BlueteethInternalNetworkStack.h
+++ BlueteethInternalNetworkStack.h
@@ -5,98 +5,224 @@
 #include <stdint.h>
 #include <functional>
 #include "HardwareSerial.h"
+#include <string>
+#include <set>
+
+using namespace std;
 
 #define MAX_PAYLOAD_SIZE (10)
+#define TOKEN_HOLD_TIME_MS (10) //Used to create a fixed token passing speed
+#define PACKET_DELAY_TIME_MS (5) //Used to create a fixed data packet transmit speed
+#define MAX_DATA_BUFFER_SIZE (40000)
 
-typedef struct {
+enum PacketType {NONE, INITIALIZAITON, CLAIM_ADDRESS, PING, SCAN, SELECT, STREAM, TEST};
 
-    bool tokenFlag;  
+class BlueteethPacket {
+
+public:
+
+    /*  Default Constructor - Sets packet to an initializaiton packet for requesting an address
+    * 
+    */
+    BlueteethPacket(){ 
+        this -> tokenFlag = 0;
+        this -> srcAddr = 255;
+        this -> dstAddr = 0;
+        this -> type = NONE;
+        for (int i = 0; i < MAX_PAYLOAD_SIZE; i++){
+            payload[i] = NULL;
+        }
+
+    }
+
+
+    /* Overloaded Constructor - Takes in inputs of the most commonly needed parameters during initialization
+    *
+    *   @tokenPacket - true = is a token packet, false = not a token packet
+    *   @srcAddr - Packet source address
+    *   @dstAddr - Packet destination address
+    */
+    BlueteethPacket(bool tokenPacket, uint8_t srcAddr, uint8_t dstAddr){
+        if (tokenPacket){
+            this -> tokenFlag = 1;
+        }
+        else this -> tokenFlag = 0;
+        this -> srcAddr = srcAddr;
+        this -> dstAddr = dstAddr;
+        this -> type = NONE;
+        for (int i = 0; i < MAX_PAYLOAD_SIZE; i++){
+            payload[i] = NULL;
+        }
+    }
+
+    uint8_t tokenFlag;  
     uint8_t srcAddr;
     uint8_t dstAddr;
     uint8_t type; 
     uint8_t payload[MAX_PAYLOAD_SIZE]; 
 
-} blueTeethPacket;
+};
+
+/* Control plane callback function
+*
+*/
+void uartFrameReceived();
+
+/* Data plane callback function
+*
+*/
+void dataStreamReceived();
 
 class BlueteethBaseStack
 {
 
 public:
 
+    friend BlueteethPacket;
+
+    /* Workaround for not being able to set a member function as a callback to serial interrupts through HardwareSerial::onRecieve
+    *
+    */
+    friend void uartFrameReceived();
+
+    /* Workaround for not being able to set a member function as a callback to serial interrupts through HardwareSerial::onRecieve
+    *
+    */
+    friend void dataStreamReceived();
+
     /* Constructor
     *
     *  @bufferSize - The size of the received packet buffer
+    *  @recieveTaskCallback - Task handle for the task to be resumed when a new packet has been received.
+    *  @controlPlane - Serial port for the control plane
+    *  @dataPlane - Serial port for the data plane
     */
-    BlueteethBaseStack(uint8_t bufferSize, HardwareSerial * controlPlane, HardwareSerial * dataPlane){
-        
-        receivedPacketBuffer = xQueueCreate(bufferSize, sizeof(blueTeethPacket));
-        transmitPacketBuffer = xQueueCreate(bufferSize, sizeof(blueTeethPacket));
+    BlueteethBaseStack(uint8_t bufferSize, TaskHandle_t * receiveTaskCallback, HardwareSerial * controlPlane, HardwareSerial * dataPlane){
+                
+        receivedPacketBuffer = xQueueCreate(bufferSize, sizeof(BlueteethPacket));
+        transmitPacketBuffer = xQueueCreate(bufferSize, sizeof(BlueteethPacket));
 
+        this -> receiveTaskCallback = receiveTaskCallback;
         this -> controlPlane = controlPlane;
         this -> dataPlane = dataPlane;
 
         this -> address = 255; //Max value (indicates that the node has not been assigned an address)
     
     }
+
+    void begin(){
+
+        this -> controlPlane -> begin(115200);
+        this -> controlPlane -> onReceive(uartFrameReceived);
+
+        this -> dataPlane -> begin(1152000, SERIAL_8N1, 18, 19); //Need to use pins 18 & 19 as Serial1 defaults literally cannot be used (they're involved in flashing and will crash your program).  
+        this -> dataPlane -> onReceive(dataStreamReceived);
+    }
     
     /* Queues a packet to be sent to the rest of the network upon receiving a token
     *
     *  @overwrite - Whether the packet buffer should be overwritten (if it is currently full)   
-    *  @return - indication of whether the buffer was written to  (0 = Did not write to packet buffer, 1 = Wrote to packet buffer)
+    *  @packet - The Blueteeth packet being queued
+    *  @return - Indication of whether the buffer was written to  (0 = Did not write to packet buffer, 1 = Wrote to packet buffer)
     */
-    bool queuePacket(bool overwrite);
+    bool queuePacket(bool overwrite, BlueteethPacket packet){
+        
+        // Serial.print("Adding packet to buffer\n\r"); //DEBUG STATEMENT
+        xQueueSend( this -> transmitPacketBuffer, &packet, 0 );
+        // Serial.print("Added packet to buffer\n\r"); //DEBUG STATEMENT
+        return true;
 
-    /* Checks to see if a packet is available.
-    *
-    *  @return - indication of whether the buffer was overwritten.  
-    */
-    bool checkForPacket(){
-
-        if (uxQueueMessagesWaiting(internalNetworkStack.transmitPacketBuffer) > 0){
-            return 1;
-        }
-        else {
-            return 0;
-        }
     }
     
-    /* Retrieves the packet at the top of the receive buffer.
+    /*  Retrieves the packet at the top of the receive buffer.
     *
-    *  @return - the packet at the top of the receive buffer.
+    *   @return - The packet at the top of the receive buffer.
     */
-    blueTeethPacket getPacket(){
-        blueTeethPacket retrievedPacket;
-        xQueueReceive(internalNetworkStack.receivedPacketBuffer, &retrievedPacket, 0);
+    BlueteethPacket getPacket(){
+        BlueteethPacket retrievedPacket;
+        xQueueReceive(this -> receivedPacketBuffer, &retrievedPacket, 0);
         return retrievedPacket;
 
     }
 
-    /* Workaround for not being able to set a member function as a callback to serial interrupts through HardwareSerial::onRecieve
+    /*  Retrieves the device's address.
     *
+    *   @return - This device's address.
     */
-    friend void packetReceived();
+    uint8_t getAddress(){
+        return this -> address;
+    }
 
-    /* Sets the ISR callbacks for the controlPlane and dataPlane interrupts
+    /*  Stream data over the data plane
+    *
+    *   @byte - The byte being transmitted;
+    */
+    void streamData(uint8_t byte){
+        this -> dataPlane -> write(byte);
+    };
+    
+    /*  Callback to perform appropriate actions upon token packet reception
     *
     */
-    friend void blueTeethNetworkSetup();
+    virtual void tokenReceived(){
+
+        while(uxQueueMessagesWaiting(this -> transmitPacketBuffer) > 0){
+                // Serial.print("Transmitted a queued packet\n\r"); //DEBUG STATEMENT
+                BlueteethPacket transmitPacket;
+                xQueueReceive(this -> transmitPacketBuffer, &transmitPacket, 0);
+                this -> transmitPacket(transmitPacket);
+                // Serial.printf("[%lu - Tx Packet] t = %u, src = %u, dst = %u, type = %u, payload = %s\n\r", millis(), transmitPacket.tokenFlag, transmitPacket.srcAddr, transmitPacket.dstAddr, transmitPacket.type, (char*) transmitPacket.payload); //DEBUG STATEMENT
+                delay(PACKET_DELAY_TIME_MS);
+        }
+
+        delay(TOKEN_HOLD_TIME_MS);
+
+        BlueteethPacket tokenPacket(true, this -> address, 254);
+        this -> transmitPacket(tokenPacket);
+        // Serial.print("Token passed\n\r"); //DEBUG STATEMENT
+    }
+
+    /*  Callback to perform appropriate actions upon initialization packet reception
+    *  
+    *   @receivedPacket - The initialization packet recevied.
+    */
+    virtual void initializationReceived(BlueteethPacket receivedPacket){
+        if (this -> address == 255){
+            this -> address = receivedPacket.payload[0];
+            
+            BlueteethPacket addressClaimPacket(false, this -> address, 0);
+            addressClaimPacket.type = CLAIM_ADDRESS;
+            addressClaimPacket.payload[0] = this -> address;
+            this -> queuePacket(1, addressClaimPacket);
+
+            receivedPacket.payload[0]++; //increment the address value and pass it on
+        }
+
+        this -> queuePacket(1, receivedPacket);
+    }
+
+    uint8_t dataBuffer[MAX_DATA_BUFFER_SIZE];
+
 
 protected:
     
-    void transmitPacket(blueTeethPacket packet){
-
+    void transmitPacket(BlueteethPacket packet){
         this -> controlPlane -> write(packet.tokenFlag);
         this -> controlPlane -> write(packet.srcAddr);
         this -> controlPlane -> write(packet.dstAddr);
-        for (int i = 0; i < MAX_PAYLOAD_SIZE; i++)
-        this -> controlPlane -> write(packet.payload[i]);
+        this -> controlPlane -> write(packet.type);
+        for (int i = 0; i < MAX_PAYLOAD_SIZE; i++){
+            this -> controlPlane -> write(packet.payload[i]);
+        }
     }
 
     uint8_t address;
+    string deviceType = "Base";
     QueueHandle_t transmitPacketBuffer;
     QueueHandle_t receivedPacketBuffer; 
     HardwareSerial * controlPlane;
     HardwareSerial * dataPlane;
+    TaskHandle_t  * receiveTaskCallback;
 
 };
 
@@ -108,28 +234,65 @@ public:
     *
     *  @bufferSize - The size of the received packet buffer
     */
-    BlueteethMasterStack(uint8_t bufferSize, HardwareSerial * controlPlane, HardwareSerial * dataPlane) : BlueteethBaseStack(bufferSize, controlPlane, dataPlane){   
+    BlueteethMasterStack(uint8_t bufferSize, TaskHandle_t * receiveTaskCallback, HardwareSerial * controlPlane, HardwareSerial * dataPlane) : BlueteethBaseStack(bufferSize, receiveTaskCallback, controlPlane, dataPlane){   
         
         this -> address = 0;
+        this -> deviceType = "Master";
 
-        blueTeethPacket initializationPacket;
-        initializationPacket.dstAddr = 255;
+        BlueteethPacket initializationPacket(false, this -> address, 255);
+        initializationPacket.type = INITIALIZAITON;
         initializationPacket.payload[0] = 1;
-        this -> transmitPacket(initializationPacket);
+        this -> queuePacket(1, initializationPacket);
     }
 
-    /* To be called from a watchdog timer task to generate a new token if one is lost.
+    /* To be called from a watchdog timer task to generate a new token if the token is lost.
     *
     */
     void generateNewToken(){
-
-        blueTeethPacket newTokenPacket;
+        BlueteethPacket newTokenPacket;
         newTokenPacket.tokenFlag = 1;
-        this -> transmitPacket(newTokenPacket);   
+        this -> transmitPacket(newTokenPacket);
     }
 
+    /* Callback to perform appropriate actions upon token packet reception
+    *
+    */
+    void tokenReceived() override {
+        this -> tokenRxFlag = true;
+        this -> BlueteethBaseStack::tokenReceived();
+    }
+
+    /* Callback to perform appropriate actions upon initialization packet reception
+    *  
+    *  @receivedPacket - The initialization packet recevied.
+    */
+    void initializationReceived(BlueteethPacket receivedPacket) override{
+        
+        if (receivedPacket.type != CLAIM_ADDRESS){
+            return; //Don't need to handle anything if the initialization pacekt is not an address claim
+        }
+
+        uint8_t newAddress = receivedPacket.payload[0];
+        if (knownAddresses.count(newAddress) == 0){
+            this -> knownAddresses.insert(receivedPacket.payload[0]);
+        }
+        else {
+            Serial.printf("Device attempted to use address %d, but it was already registered to another device\n\r", newAddress);
+        }
+    }
+
+    bool getTokenRxFlag(){
+        return this -> tokenRxFlag;
+    }
+
+    void resetTokenRxFlag(){
+        tokenRxFlag = false;
+    }
 
 private:
+
+    bool tokenRxFlag;
+    set<uint8_t> knownAddresses;
     
 };
 
